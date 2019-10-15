4627d669244061ba3498532fedddb811d1e521fe
Edward Lee <elee5696@gmail.com>
2019-10-15 00:33:39 +0000

    routing endpoint complete, add endpoint WIP, conn info

---
+++ server/public/api/project.php
@@ -0,0 +1,24 @@
+<?php
+define('INTERNAL', true);
+
+require_once('db_connection.php');
+require_once('functions.php');
+
+if(!$conn) {
+  throw new Exception('connection failed');
+}
+
+method = $_SERVER['REQUEST_METHOD'];
+
+switch($method) {
+  case 'GET':
+    require('project_get.php');
+    break;
+  case 'POST':
+    require('project_add.php');
+    break;
+  default:
+    throw new Exception('Cannot ' . $method . ' /api/project.php');
+}
+
+?>
