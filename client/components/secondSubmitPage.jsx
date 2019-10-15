80bfc9f4f834f644f830bea0a3714a2a7c6f2dc3
Brena Patel <brenapatel@gmail.com>
2019-10-15 17:25:23 +0000

    added functionality to goals input box

--- client/components/secondSubmitPage.jsx
+++ client/components/secondSubmitPage.jsx
@@ -4,22 +4,32 @@ class SecondSubmit extends React.Component {
 constructor(props){
   super(props);
   this.state = {
-    goals: "",
-    materials: "",
-    SetUp: "",
-    Outcomes: "",
+    info: [
+      {"id": "",
+      "name": "",
+      "description": "",
+      "set-up": "",
+      "outcomes": "",
+      "rating": null,
+      "image": "",
+      "goals": []
+      }]
   };
   this.handleGoalChange = this.handleGoalChange.bind(this);
+  this.handleSubmitChange = this.handleSubmitChange.bind(this);
 }
 
 
 
 handleGoalChange(event){
   this.setState({goals: event.target.value });
-  let goals = this.state.goals
+
 }
 
-
+handleSubmitChange(){
+  console.log(this.state.goals);
+  event.preventDefault();
+}
 
 
 
@@ -27,35 +37,35 @@ handleGoalChange(event){
     return (
       <div>
       <div>
-        <form class="form-inline">
-          <div class="form-group row"> </div>
-            <label for="colFormLabel" class="col-sm-2 col-form-label">Goals</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="goalSubmit" placeholder="Goals Entry"></input>
-            <button type="button" class="btn btn-secondary">Submit</button>
+        <form className="form-inline">
+          <div className="form-group row"> </div>
+            <label for="colFormLabel" className="col-sm-2 col-form-label">Goals</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="goalSubmit" onChange={this.handleGoalChange} value={this.state.goals} placeholder="Goals Entry"></input>
+            <button type="button" onClick={this.handleSubmitChange} className="btn btn-secondary">Submit</button>
           </div>
         </form>
       </div>
       <div>
-        <form class="form-inline">
-          <div class="form-group row"> </div>
-          <label for="colFormLabel" class="col-sm-2 col-form-label">Materials</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="materialSubmit" placeholder="Materials Entry"></input>
-            <button type="button" class="btn btn-secondary">Submit</button>
+        <form className="form-inline">
+          <div className="form-group row"> </div>
+          <label for="colFormLabel" className="col-sm-2 col-form-label">Materials</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="materialSubmit" placeholder="Materials Entry"></input>
+            <button type="button" className="btn btn-secondary">Submit</button>
           </div>
         </form>
       </div>
         <form>
-          <div class="form-group">
+          <div className="form-group">
             <label for="SetUp">Set-Up</label>
             <input type="text" className="form-control setUpEntry" id="setUp" placeholder="Setup Entry" />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="Outcome">Outcomes</label>
             <textarea className="form-control outcomeBox" id="outcomeBox" placeholder="Outcome Entries" rows="3"></textarea>
           </div>
-          <button type="button" class="btn btn-secondary btn-lg">Next</button>
+          <button type="button" className="btn btn-secondary btn-lg">Next</button>
         </form>
       </div>
     );
