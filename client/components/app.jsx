93a9a3e70dcde37963d4db1dc45d52fac385ad49
anjaleena <anjaleenab@github.com>
2019-10-15 13:32:49 -0700

    made the materials and goal arrays have functionality

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './header';
 import Projectsubmit from './projectSubmit';
-import SecondSubmit from './secondSubmitPage';
 
 export default class App extends React.Component {
   render() {
