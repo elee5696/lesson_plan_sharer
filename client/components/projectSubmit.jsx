bc54e3bf6bd7235d060d11d7a671416ad34a49ec
porinalena <porinalena@gmail.com>
2019-10-15 12:53:00 -0700

    Defined ProjectList, ProjectDetails and ProjectListItem Components; fetched data from the DB to show the projects list

--- client/components/projectSubmit.jsx
+++ client/components/projectSubmit.jsx
@@ -5,15 +5,15 @@ class Projectsubmit extends React.Component {
     return (
 
       <form>
-        <div class="form-group">
-          <label for="exampleFormControlInput1">Project Title</label>
+        <div className="form-group">
+          <label>Project Title</label>
           <input type="text" className="form-control projectEntry" id="exampleFormControlInput1" placeholder="Project Entry"/>
   </div>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Description</label>
+          <div className="form-group">
+            <label>Description</label>
             <textarea className="form-control descriptionBox" id="exampleFormControlTextarea1" placeholder="Description Text" rows="3"></textarea>
           </div>
-        <button type="button" class="btn btn-secondary btn-lg">Next</button>
+        <button type="button" className="btn btn-secondary btn-lg">Next</button>
 </form>
 
     );
