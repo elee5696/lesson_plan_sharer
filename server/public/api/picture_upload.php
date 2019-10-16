6c2de7f710dfef1e2cbebd747efbbe957534e597
Edward Lee <elee5696@gmail.com>
2019-10-16 04:49:09 +0000

    user can upload, redirects to blacnk page

--- server/public/api/picture_upload.php
+++ server/public/api/picture_upload.php
@@ -1,18 +1,16 @@
 <?php
 
-// $temp =sys_get_temp_dir();
+if($_FILES['picture']) {
 
-// echo $temp;
-// echo php_ini_loaded_file();
-
-if(isset($_FILES['image'])) {
   $errors=0;
-  $picture_uploaded = $_FILES['image']['name'];
-  $pictureSize = $_FILES['image']['size'];
-  $picture_tempLocation = $_FILES['image']['tmp_name'];
-  $file_type = $_FILES['image']['type'];
-  $file_ext= strtolower(end(explode('.', $_FILES['image']['name'])));
+  $picture_uploaded = $_FILES['picture']['name'];
+  $pictureSize = $_FILES['picture']['size'];
+  $picture_tempName = $_FILES['picture']['tmp_name'];
+  $file_type = $_FILES['picture']['type'];
 
+  $filearray = explode('.', $picture_uploaded);
+  $fileend = end($filearray);
+  $file_ext= strtolower($fileend);
   $extentions = array("jpeg", "jpg", "png");
 
   if(in_array($file_ext, $extentions)=== false){
@@ -20,13 +18,13 @@ if(isset($_FILES['image'])) {
     throw new Exception('Image must be in JPEG or PNG format');
   }
 
-  if($file_size > 2097152 ) {
+  if($pictureSize > 2097152 ) {
     $errors++;
     throw new Exception('File is too large. Please upload a photo smaller than 5 MB');
   }
 
   if(empty($errors)==true){
-    move_uploaded_file($picture_tempLocation, "/images/" . $picture_uploaded);
+    move_uploaded_file($picture_tempName, "../images/$picture_uploaded");
     echo "File Uploaded";
   } else {
     throw new Exception('File could not be uploaded');
