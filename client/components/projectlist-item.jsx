6cc9f3a483186fe61fc1c9ea34db258d044e5974
porinalena <porinalena@gmail.com>
2019-10-16 17:01:25 -0700

    Deleted button from the project tile; added styling to the home page

--- client/components/projectlist-item.jsx
+++ client/components/projectlist-item.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 export default class ProjectListItem extends React.Component {
   render() {
     return (
-      <div className="card card-container" style={{width: '60rem'}}>
-        <div className="row">
-          <div className="col">
-            <img className="card-img-top list-image" src={this.props.projectImage} alt="Card image cap"/>
-          </div>
-          <div className="col card-body-container">
+
+      <div className="project-tile-container ml-3 mb-3 d-flex justify-content-between" onClick={() => this.props.view('details', { id: this.props.id })}>
+      <div className="card" style={{width: '15rem'}}>
+          <div className="project-tile-image-container">
+            <img className="card-img-top" style={{ height: '10rem' }} src={this.props.projectImage} alt="Card image cap"/>
+        </div>
           <div className="card-body">
             <h5 className="card-title">{this.props.projectName}</h5>
-            <p className="card-text">{this.props.projectDescription}</p>
-              <a href="#" onClick={() => this.props.view('details', { id: this.props.id })} className="btn btn-outline-dark">View Details</a>
-          </div>
+            <p className="card-text project-tile-text">{this.props.projectDescription}.</p>
+            {/* <a href="#" onClick={() => this.props.view('details', { id: this.props.id })} className="btn btn-outline-dark project-tile-button">Learn More</a> */}
           </div>
 </div>
 </div>
+
+
     );
   }
 }
