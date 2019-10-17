5c88b143adac5d59ec0e238149a28216c99a0756
anjaleena <anjaleenab@github.com>
2019-10-16 17:07:42 -0700

    redirected user after upload

--- client/components/picture-upload.jsx
+++ client/components/picture-upload.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 class PictureUploadForm extends React.Component {
   constructor(props) {
     super(props);
-    //this.onSubmit = this.onSubmit.bind(this);
+    this.state= {
+      inputValue: null
+    }
+    // this.saveUrl = this.saveUrl.bind(this);
+    // this.testState = this.testState.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
   }
 
   // onSubmit(event) {
@@ -14,6 +19,14 @@ class PictureUploadForm extends React.Component {
   //   });
   // }
 
+  // testState(){
+  //   console.log(this.state.inputValue);
+  // }
+  // saveUrl(event) {
+  //   this.setState({
+  //    inputValue: event.target.value
+  //   });
+  // }
   render() {
     return (
       <form action="/api/picture_upload.php" method="POST" encType="multipart/form-data">
