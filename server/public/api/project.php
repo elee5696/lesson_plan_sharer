8aee9564c4a50de7628d813768f4c43dbcb319f2
anjaleena <anjaleenab@github.com>
2019-10-17 15:52:34 -0700

    modified get endpoint

--- server/public/api/project.php
+++ server/public/api/project.php
@@ -15,8 +15,8 @@ switch($method) {
     require('project_get.php');
     break;
   case 'POST':
-    require('project_add.php');
     require('picture_upload.php');
+    require('project_add.php');
     break;
   default:
     throw new Exception('Cannot ' . $method . ' /api/project.php');
