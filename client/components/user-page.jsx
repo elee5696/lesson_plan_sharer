397bbd2abd9ab9089bd0b8b2044ff349ba03a3f1
porinalena <porinalena@gmail.com>
2019-10-20 21:23:29 -0700

    Removed back button link from user page

--- client/components/user-page.jsx
+++ client/components/user-page.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default class UserPage extends React.Component {
   render() {
     return (
       <div className="entire-page-container container">
-        <div className="back-button-container row mt-3 mb-3 col-12">
-          <Link to="/provs" className="back-button col-10" style={{ textDecoration: 'none', color: 'black' }}>{'< Back'}</Link>
-        </div>
         <div className="spacer col col-1 p-0"></div>
         <div className="user-pic-enter-info-container col-12 row p-0 m-0">
           <div className="pic col-6 p-0">
