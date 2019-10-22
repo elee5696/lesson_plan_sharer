1d31939fa17ef2906cf9733cad4a29f25a7293a4
porinalena <porinalena@gmail.com>
2019-10-22 11:19:50 -0700

    Fixed bubbles sizing issue, centered carousel

--- client/components/project-details.jsx
+++ client/components/project-details.jsx
@@ -42,14 +42,14 @@ export default class ProjectDetails extends React.Component {
           <div className="project-image-container row justify-content-center mb-3">
             <img
               className="project-image p-0"
-              style={{ width: '90%' }}
+              style={{ width: '90%', maxWidth: '450px' }}
               src={this.state.project.image}>
             </img>
           </div>
           <div className="project-title-container col-10 mb-3 p-0">
             <h1 className="project-title">{this.state.project.name}</h1>
           </div>
-          <div className="project-desc-container col-10 mb-3">
+          <div className="project-desc-container col-12 mb-3">
             <div className="project-desc-header-container row mb-2">
               <h1 className="project-desc-header">Description</h1>
             </div>
@@ -61,7 +61,7 @@ export default class ProjectDetails extends React.Component {
             <div className="project-goals-header-container row mb-2 ml-1">
               <h1 className="project-goals-header">Goals</h1>
             </div>
-            <div className="row">
+            <div className="row container">
               <div className="project-goals-list row m-0">
                 {this.state.project.goals.map((e, i) => {
                   return (
@@ -69,14 +69,15 @@ export default class ProjectDetails extends React.Component {
                       key={i}
                       className="project-goals-list-item"
                       text={e}
-                      width="80px" />
+                      minWidth="120px"
+                      maxWidth="120px" />
                   );
                 })}
               </div>
             </div>
           </div>
           <div className="project-setup-materials-container col-12 row p-0 m-0">
-            <div className="set-up col-6 p-0">
+            <div className="set-up p-0 col-5">
               <div className="project-setup-header-container col mb-2 p-0">
                 <h1 className="project-setup-header">Set-Up</h1>
               </div>
@@ -94,11 +95,11 @@ export default class ProjectDetails extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="materials col-6 p-0">
-              <div className="project-materials-header-container col mb-2">
+            <div className="materials p-0">
+              <div className="project-materials-header-container col mb-2 p-0">
                 <h1 className="project-materials-header">Materials</h1>
               </div>
-              <div className="project-materials-container col-12 mb-3">
+              <div className="project-materials-container mb-3">
                 <div className="project-materials-list-container">
                   {this.state.project.materials.map((e, i) => {
                     return (
@@ -106,30 +107,31 @@ export default class ProjectDetails extends React.Component {
                         key={i}
                         className="project-materials-list-item"
                         text={e}
-                        width="150px" />
+                        minWidth="150px"
+                        maxWidth="150px" />
                     );
                   })}
                 </div>
               </div>
             </div>
-            <div className="project-outcomes-container">
-              <div className="project-outcomes-header-container row mb-2">
-                <h1 className="project-outcomes-header col">Outcomes</h1>
-              </div>
-              <div className="project-outcomes-body row mb-4">
-                <h5 className="col project-outcomes-text">{this.state.project.outcomes}</h5>
-              </div>
+          </div>
+          <div className="project-outcomes-container">
+            <div className="project-outcomes-header-container row mb-2">
+              <h1 className="project-outcomes-header col">Outcomes</h1>
             </div>
-            <div className="project-rating-container col-10 mb-2 p-0">
-              <div className="project-rating-header-container row">
-                <h1 className="project-rating-header col-10">Feedback</h1>
-              </div>
-              <div className="project-rating-body mb-2">
-                <Ratings
-                  id={this.state.project.id}
-                  rating={this.state.project.rating}
-                  rating_count={this.state.project.rating_count}/>
-              </div>
+            <div className="project-outcomes-body row mb-4">
+              <h5 className="col project-outcomes-text">{this.state.project.outcomes}</h5>
+            </div>
+          </div>
+          <div className="project-rating-container col-10 mb-2 p-0">
+            <div className="project-rating-header-container row">
+              <h1 className="project-rating-header col-10">Feedback</h1>
+            </div>
+            <div className="project-rating-body mb-2">
+              <Ratings
+                id={this.state.project.id}
+                rating={this.state.project.rating}
+                rating_count={this.state.project.rating_count}/>
             </div>
           </div>
         </div>
