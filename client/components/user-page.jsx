594b217394fbf913127acb6c0e9355ab3c8f3d19
Edward Lee <elee5696@gmail.com>
2019-10-28 20:34:52 +0000

    user cannot edit profiles other users profiles

--- client/components/user-page.jsx
+++ client/components/user-page.jsx
@@ -84,16 +84,24 @@ export default class UserPage extends React.Component {
                   this.state.modal && <UserPhotoUpdate/>
                 }
               </div>
-              <button
-                type="button"
-                className="btn editUserButton btn-sm shadow-none row ml-2"
-                onClick={this.handleUpdateModal}>
-                Edit
-              </button>
+              {
+                this.state.user.id === this.props.currentUser.id
+                  ? <button
+                    type="button"
+                    className="btn editUserButton btn-sm shadow-none row ml-2"
+                    onClick={this.handleUpdateModal}>
+                  Edit
+                  </button>
+                  : null
+              }
             </div>
-            <div className="enter-info col-10 p-0">
-              <p className="enter-info-text ml-4 mt-2">Enter your info and add an optional profile picture.</p>
-            </div>
+            {
+              this.state.user.id === this.props.currentUser.id
+                ? <div className="enter-info col-10 p-0">
+                  <p className="enter-info-text ml-4 mt-2">Edit your info or your profile picture.</p>
+                </div>
+                : null
+            }
           </div>
           <div className="personal-info-container mb-4 mt-3">
             <h2 className="personal-info-text">Personal Info</h2>
@@ -112,13 +120,17 @@ export default class UserPage extends React.Component {
               : <div className="username-key-value-container col ml-0 mb-3">
                 <div className="username-key row p-0">
                   <p className="col-8 p-0 m-0">Username: </p>
-                  <button
-                    id="username"
-                    onClick={this.editProject}
-                    type="button"
-                    className="btn editUserButton col-4 btn-sm shadow-none">
-                    Edit
-                  </button>
+                  {
+                    this.state.user.id === this.props.currentUser.id
+                      ? <button
+                        id="username"
+                        onClick={this.editProject}
+                        type="button"
+                        className="btn editUserButton col-4 btn-sm shadow-none">
+                      Edit
+                      </button>
+                      : null
+                  }
                 </div>
                 <div className="username-value row p-0">
                   <p className="username-value-text col p-0 m-0">
@@ -144,13 +156,17 @@ export default class UserPage extends React.Component {
               : <div className="name-key-value-container col ml-0 mb-3">
                 <div className="name-key row p-0">
                   <p className="col-8 p-0 m-0">Name: </p>
-                  <button
-                    id="name"
-                    onClick={this.editProject}
-                    type="button"
-                    className="btn editUserButton col-4 btn-sm shadow-none">
-                Edit
-                  </button>
+                  {
+                    this.state.user.id === this.props.currentUser.id
+                      ? <button
+                        id="name"
+                        onClick={this.editProject}
+                        type="button"
+                        className="btn editUserButton col-4 btn-sm shadow-none">
+                      Edit
+                      </button>
+                      : null
+                  }
                 </div>
                 <div className="name-value row p-0">
                   <p className="name-value-text col p-0 m-0">
@@ -176,13 +192,17 @@ export default class UserPage extends React.Component {
               : <div className="experience-key-value-container col ml-0 mb-3">
                 <div className="experience-key row p-0">
                   <p className="col-8 p-0 m-0">Experience:</p>
-                  <button
-                    type="button"
-                    id="years"
-                    onClick={this.editProject}
-                    className="btn editUserButton col-4 btn-sm shadow-none">
-                      Edit
-                  </button>
+                  {
+                    this.state.user.id === this.props.currentUser.id
+                      ? <button
+                        type="button"
+                        id="years"
+                        onClick={this.editProject}
+                        className="btn editUserButton col-4 btn-sm shadow-none">
+                        Edit
+                      </button>
+                      : null
+                  }
                 </div>
                 <div className="experience-value row p-0">
                   <p className="experience-value-text col p-0 m-0">
@@ -208,13 +228,17 @@ export default class UserPage extends React.Component {
               : <div className="about-me-key-value-container col ml-0 mb-3">
                 <div className="about-me-key row p-0">
                   <p className="col-8 p-0 m-0">About Me:</p>
-                  <button
-                    id="about_me"
-                    onClick={this.editProject}
-                    type="button"
-                    className="btn editUserButton col-4 btn-sm shadow-none">
-                  Edit
-                  </button>
+                  {
+                    this.state.user.id === this.props.currentUser.id
+                      ? <button
+                        id="about_me"
+                        onClick={this.editProject}
+                        type="button"
+                        className="btn editUserButton col-4 btn-sm shadow-none">
+                      Edit
+                      </button>
+                      : null
+                  }
                 </div>
                 <div className="about-me-value row p-0">
                   <p className="about-me-text col p-0 m-0">
@@ -256,12 +280,16 @@ export default class UserPage extends React.Component {
             </div>
           </div>
           <div>
-            <button
-              type="button"
-              className="btn searchButton shadow-none mt-4"
-              onClick={this.logOut}>
-                  Log-out
-            </button>
+            {
+              this.state.user.id === this.props.currentUser.id
+                ? <button
+                  type="button"
+                  className="btn searchButton shadow-none mt-4"
+                  onClick={this.logOut}>
+                Log-out
+                </button>
+                : null
+            }
           </div>
         </div>
         <div className="spacer col-1 p-0"></div>
