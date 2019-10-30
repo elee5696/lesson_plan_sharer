fd39a6b8026392238f1c7e967be644b7b0cbcd63
anjaleena <anjaleenab@github.com>
2019-10-30 12:33:42 -0700

    added the x to each list bubble

--- client/components/list-bubble.jsx
+++ client/components/list-bubble.jsx
@@ -8,12 +8,15 @@ export default class ListBubble extends React.Component {
 
   deleteBubble(event) {
     if (event.currentTarget.id === 'somegoal') {
-      this.props.deleteGoal(event.currentTarget.innerHTML);
+      this.props.deleteGoal(this.props.text);
     } else if (event.currentTarget.id === 'somematerial') {
-      this.props.deleteMaterial(event.currentTarget.innerHTML);
+      this.props.deleteMaterial(this.props.text);
     }
   }
   render() {
+    let hasCallbackFunction = this.props.deleteGoal || this.props.deleteMaterial;
+    let crossMark = '     ❌';
+
     let borderStyle = {
       border: '1px #E5E5E5 solid',
       background: '#FFC7F9',
@@ -26,10 +29,22 @@ export default class ListBubble extends React.Component {
     return (
       <div className="bubble-container col justify-content-center m-1 p-1" style={borderStyle}>
         <div className="text-container">
+
           <div onClick={this.deleteBubble}
             id={this.props.id}
-            className="m-0 goals-bubble-text"
-            style={{ fontSize: '14px' }}>{this.props.text}</div>
+            className={
+              hasCallbackFunction
+                ? ' m-0 goals-bubble-text'
+                : 'm-0 goals-bubble-text'
+            }
+            style={{ fontSize: '14px' }}>{this.props.text}
+            <small style ={{ fontSize: '8px' }}>{
+              hasCallbackFunction
+                ? crossMark
+                : null
+            }</small>
+          </div>
+
         </div>
       </div>
     );
